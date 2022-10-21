89e222aab6204bea552f6aaf2780d0edd105a36b
barru <barru@motionpay.id>
2022-10-21 21:43:41 +0700

    updated

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"os"
 )
 
-/*
-Barru Kurniawan
-My Gram - Final Project
-GLN038MNC007
-*/
+// @title Barru Final Project
+// @description Final Project Golang
+// @version v1.0
+// @termsOfService http://swagger.io/terms/
+// @BasePath /
+// @host finalproject-production-2281.up.railway.app
+// @contact.name Barru Kurniawan
+// @contact.email barru.kurniawan@gmail.com
 
 func main() {
 	var PORT = os.Getenv("PORT")
